37a4bc028c9d0f5b07feee88542491b042dac16d
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-06-17 11:20:18 -0300

    Ex 08 - Ajuste na exibição do navbar-toggler.

--- Ex8 - Trabalho Final/tech-news/js/view.js
+++ Ex8 - Trabalho Final/tech-news/js/view.js
@@ -5,7 +5,7 @@ function loadContentView(cod){
         if(news.cod == cod){
             $("#articleTitle").html(news.title);
 
-            let newElement =  "<div class='row' style='width: 75%;'>";
+            let newElement =  "<div class='row' id='news-body'>";
                 newElement += "<img src='"+ news.urlImgCover +"' class='card-img-top'>";                        
                 newElement += "<div class='card-body' id='view-card-body'>";
                 newElement += "<div class='small text-muted' id='view-news-date'>"+ news.date +"</div>";    
